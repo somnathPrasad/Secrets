9df6d3144af898a0a1d75b427b7564a0418a876d
Somnath Prasad <somnathprasad559@gmail.com>
2020-11-28 19:38:45 +0530

    Level 5 (hashing salting and using cookies to save sessions)

--- app.js
+++ app.js
@@ -4,27 +4,53 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 app.use(express.static("public"));
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+  secret: "Our little secret.",
+  resave:false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex",true);
 
 const userSchema = new mongoose.Schema({
   email:String,
   password:String
 });
 
+userSchema.plugin(passLocalMongoose);
+
 const User = new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 ////////////////////////////ROUTES TO HOME PAGE/////////////////////////////////////////
 app.get("/",function(req,res){
   res.render("home");
 });
+
+////////////////////////////ROUTES TO SECRETS PAGE/////////////////////////////////////////
+app.route("/secrets")
+
+.get(function(req,res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }else{
+    res.redirect("/login");
+  }
+})
 
 ////////////////////////////ROUTES TO REGISTER PAGE/////////////////////////////////////////
 app.route("/register")
@@ -34,21 +60,16 @@ app.route("/register")
 })
 
 .post(function(req,res){
-
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User({
-      email:req.body.username,
-      password:hash
-    });
-    newUser.save(function(err){
-      if(!err){
-        res.render("secrets");
-      }else {
-        console.log(err);
-      }
-    });
-});
-
+  User.register({username:req.body.username},req.body.password,function(err,user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    }else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets")
+      })
+    }
+  })
 });
 
 ////////////////////////////ROUTES TO LOGIN PAGE/////////////////////////////////////////
@@ -59,25 +80,25 @@ app.route("/login")
 })
 
 .post(function(req,res){
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({email:username},function(err,foundUser){
+  const user = new User({
+    username:req.body.username,
+    password:req.body.password
+  });
+  req.login(user,function(err){
     if(err){
       console.log(err);
     }else{
-      if(foundUser){
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-          if(result === true){
-            res.render("secrets");
-          }
-          });
-        }
-      }
-
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets")
+      });
+    }
   })
 });
-
-
+////////////////////////////ROUTES TO LOGOUT PAGE/////////////////////////////////////////
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+})
 
 
 
